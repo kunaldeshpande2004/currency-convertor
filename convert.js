514cd05813a9137e5f4d1d4d45e80dd2dbe73395
kunaldeshpande2004 <165461000+kunaldeshpande2004@users.noreply.github.com>
2024-03-30 13:50:51 +0530

    Update convert.js

--- convert.js
+++ convert.js
@@ -1,52 +1,51 @@
-const BASE_URL ="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-const dropdown = document.querySelectorAll(".dropdown select");
-let btn = document.querySelector("button");
-let fromcurr=document.querySelector(".from select");
-let tocurr=document.querySelector(".to select");
-let msg=document.querySelector(".msg")
-for(let select of dropdown)
-{
-    for(let currcode in countryList)
-    {
-        let opt=document.createElement("option");
-        opt.innerText=currcode;
-        opt.value=currcode;
-        if(select.name==="from" && currcode === "USD")
-        {
-            opt.selected="selected";
-        } else if(select.name==="to" && currcode === "INR")
-        {
-            opt.selected="selected"
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+// Populate dropdowns with currency codes
+for (let select of dropdowns) {
+    for (let currcode in countryList) {
+        let opt = document.createElement("option");
+        opt.innerText = currcode;
+        opt.value = currcode;
+        if (select.name === "from" && currcode === "USD") {
+            opt.selected = "selected";
+        } else if (select.name === "to" && currcode === "INR") {
+            opt.selected = "selected";
         }
         select.append(opt);
     }
-    select.addEventListener("change",(evt)=>{
+    select.addEventListener("change", (evt) => {
         updateFlag(evt.target);
     });
 }
-const updateFlag=(element)=>{
-    let currCode=element.value;
+
+// Update flag images based on selected currency code
+const updateFlag = (element) => {
+    let currCode = element.value;
     let countryCode = countryList[currCode];
-    let src= `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let src = `https://flagsapi.com/${countryCode}/flat/64.png`;
     let img = element.parentElement.querySelector("img");
-    img.src=src;
+    img.src = src;
 };
-btn.addEventListener("click",async (evt)=>{
+
+btn.addEventListener("click", async (evt) => {
     evt.preventDefault();
-    let amount = document.querySelector("input");
-    let amountval=amount.value;
-    if(amountval===""||amountval<1){
-        amountval=1;
-        amount.value="1";
+    let amount = document.querySelector("input").value;
+    if (amount === "" || amount < 1) {
+        amount = 1;
     }
-    const URL =`${BASE_URL}/${fromcurr.value.toLowerCase()}/${tocurr.value.toLowerCase()}.json`;
-     let response = await fetch(URL);
-     
-     let data = await response.json();
-     
-     let rate=data[tocurr.value.toLowerCase()];
-     
-     let finalamount=amountval*rate;
-     console.log(finalamount);
-     msg.innerText=`${amountval} ${fromcurr.value}=${finalamount} ${tocurr.value}`;
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+    let finalAmount = amount * rate;
+    msg.innerText = `${amount} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
 });
+
+// Initial flag update based on default selected currencies
+updateFlag(fromCurr);
+updateFlag(toCurr);
